fba23356704a131460c83a23374e0bc0b322efb4
casszhao <cass790412@gmail.com>
2021-08-14 03:06:43 +0100

    added get the top words testing

--- Multi-choice.py
+++ Multi-choice.py
@@ -6,11 +6,15 @@ import nltk
 
 choice_list = pd.read_csv('./data/sorted_cats.csv', header=None)[0].to_list()
 print(choice_list)
+cate_dic = {k: v for k, v in enumerate(choice_list)}
+print(cate_dic)
+
 print(len(choice_list))
 print(len(str(choice_list)))
 testing_data = pd.read_csv('./data/new_cat_list_3match_results.csv', usecols=[2,3,6])
 print(testing_data)
 print(len(testing_data))
+
 text_list = testing_data['Abstract'].to_list()
 
 model_name = 'emilyalsentzer/Bio_ClinicalBERT'
@@ -21,10 +25,15 @@ def get_prompt_list(prompt):
     prompt_list = []
     for i in range(len(choice_list)):
         prompt_list.append(prompt)
-    print(len(prompt_list))
     return prompt_list
 
+def num2cat_dic(indices_list):
+    map_cat = []
+    for ind in indices_list:
+        cat_value = cate_dic[ind]
+        map_cat.append(cat_value)
 
+    return map_cat
 
 prediction_list = []
 for prompt in text_list:
@@ -37,12 +46,18 @@ for prompt in text_list:
     outputs = model(**{k: v.unsqueeze(0) for k, v in encoding.items()}, labels=labels)  # batch size is 1
 
     # the linear classifier still needs to be trained
-    loss = outputs.loss
-    logits = outputs.logits
+    loss = outputs[0]
+    logits = outputs[1] # shape (1, sorted_cates)
 
-    prediction_list.append(logits)
+    values, indices = tf.nn.top_k(logits, 5)
+
+    indices = indices.numpy().flatten() #numpy.ndarray
+    cate = num2cat_dic(indices)
+    prediction_list.append(cate)
 
 testing_data['prediction'] = prediction_list
+
+
 
 testing_data.to_csv('./data/testing_results.csv')
 
--- testing.py
+++ testing.py
@@ -1,5 +1,12 @@
 import torch
 import tensorflow as tf
+import pandas as pd
+
+choice_list = pd.read_csv('./data/sorted_cats.csv', header=None)[0].to_list()
+print(choice_list)
+cate_dic = {k: v for k, v in enumerate(choice_list)}
+print(cate_dic)
+
 
 x = torch.rand(1, 3)
 print('the tensor is')
@@ -8,5 +15,17 @@ print(x)
 values, indices = tf.nn.top_k(x,2)
 print('------value ')
 print(values)
+print('')
 print('------indices ')
 print(indices)
+indices = indices.numpy().flatten()
+print(indices)
+# print(indices.type())
+
+def num2cat_dic(indices_list):
+    map_cat = []
+    for ind in indices_list:
+        cat_value = cate_dic[ind]
+        map_cat.append(cat_value)
+
+    return map_cat
